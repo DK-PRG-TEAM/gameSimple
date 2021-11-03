b7311cfa17a7c5a882bf090a37a8c81711721490
anyingiit <49945850+anyingiit@users.noreply.github.com>
2021-11-03 16:17:36 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Player anying = new Player("某同学", "00000000000");
+        Game game = new Game(anying);
+        game.printGameList();
+        Scanner sin = new Scanner(System.in);
+        System.out.print("请输入游戏序号: ");
+        game.startGameFromNumber(sin.nextInt());
+    }
+}
